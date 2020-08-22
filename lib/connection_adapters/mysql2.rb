ddaaba0c0387c055d518c67cc5d6defa416e2075
Stuart Mathews <mathewsst@vmware.com>
2020-08-22 13:43:10 +0000

    Add support for automatically Reverting back to using Mysql2Adapter after Rails 5 to ensure it contiues to work in the future

--- lib/connection_adapters/mysql2.rb
+++ lib/connection_adapters/mysql2.rb
@@ -2,7 +2,18 @@ require 'active_record/connection_adapters/mysql2_adapter'
 
 module ActiveRecord
   module ConnectionAdapters
-    existing_class = ActiveRecord::VERSION::MAJOR < 4 && defined?( Mysql2Adapter ) ? Mysql2Adapter : AbstractMysqlAdapter
+
+    # To support activerecord-mysql-awesome used in cloudpercept using Rails 4.2, we want to use AbstractMySqlAdapter. 
+    # Its safe to do so: https://github.com/iangreenleaf/native_enum/pull/9  
+    # This in theory can be done until when its not feasible anymore ie AR 6 when Mysql2Adapter has to be used
+    # and by that point active-record-mysql-awesome should be long gone and we can use a prestine version
+    # of native_enum moving forward...ie update your gemfile accordingly  
+    # We want to use AbstractMySqlAdapter because this has been correctly monkey patched by activerecord-mysql-awesome into include
+    # the appropriate enhancements from AR 5.0 backported to 4.x
+    puts '** native_enum: activerecord-mysql-awesome is no longer required in Rails 5. Please remove it, and also use a new, unpatched version of native_enum from https://github.com/iangreenleaf/native_enum to remove this message **' if ActiveRecord::VERSION::MAJOR > 4
+    can_use_abstract_adapter = ActiveRecord::VERSION::MAJOR > 3 && ActiveRecord::VERSION::MAJOR < 6
+    use_preferable_adapter = defined?(Mysql2Adapter) ? Mysql2Adapter : AbstractMysqlAdapter
+    existing_class = can_use_abstract_adapter ? AbstractMysqlAdapter : use_preferable_adapter
 
     existing_class.class_eval do
       def native_database_types_with_enum
